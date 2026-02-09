89a1b95a3b85e65d8c279f4c8f0f4abdce7063aa
Rene-VillaSnork <rene.villatapia@snorkel.ai>
2026-02-09 03:57:19 -0600

    Update TrainingMaterial.jsx

--- src/components/sections/TrainingMaterial.jsx
+++ src/components/sections/TrainingMaterial.jsx
@@ -7,6 +7,13 @@ function TrainingMaterial() {
       <div className="section-card">
         <ul className="sample-list">
           <li>
+            <a className="inline-link"
+              href="https://dataverse.harvard.edu/dataverse/harvard?q=&fq1=license%3A%22CC0+1.0%22&fq0=dvObjectType%3A%28dataverses+OR+datasets%29&types=dataverses%3Adatasets&sort=dateSort&order="
+              target="_blank" rel="noreferrer">
+              Harvard Dataverse
+            </a>
+          </li>
+          <li>
             <a
               className="inline-link"
               href="https://docs.google.com/document/d/1XLnoZRM5wXWNWp_8PtHQNFntOykjFwrNR2M8rQPqOF8/edit?tab=t.4fr5gdls7wir"
